9939aca4908347dfae754839bf5b4788f4980116
Boris G <borisgoz>
2020-11-03 21:56:22 +0000

    colors

---
+++ prints.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "starting"
+echo
+echo -e "\e[0;31m ERROR \e[0m"
+echo -e "\e[7;35m\e[1;47m ERROR - check file \e[0m"
+echo -e "\e[7;35m\e[1;47m ERROR - check file \e[0m" > filee
+echo -e "\033[1;31mThis is bold red text\033[0m"
+echo -e "\033[0;31mThis is bold red text\033[0m"
+echo -e "\033[5;31mThis is bold red text\033[0m"
+echo -e "\033[7;31mThis is bold red text\033[0m"
+echo "ending"
