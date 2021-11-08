bd6a690058bc37a682d5c5a5512aecdc1517209c
Boris Goozman <boris@goozman>
2021-11-08 21:18:24 +0000

    vms

---
+++ gen2/vms/boris.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#start VM1
+RESON=0
+MSG="All done, VM "$NAME" is up..."
+
+NAME=vm1
+RAM_MB=256M
+MAX_RAM_MB=512M
+CORE_COUNT=1
+VM_RAW=/VMs/HD/VM1.raw
+KERNEL=/VMs/kernel/vmlinuz-5.10.52-gentoo
+ETH0_NAME="$NAME"_eth0
+#ETH1_NAME="$NAME"_eth1
+
+CMD="qemu-system-x86_64 -enable-kvm \
+			-cpu host \
+			-name $NAME \
+			-nographic \
+			-smp $CORE_COUNT \
+			-m size=$RAM_MB,maxmem=$MAX_RAM_MB \
+			-nic tap,ifname=$ETH0_NAME,mac=08:00:27:00:01:01,script=no,downscript=no &
+"
+echo $CMD
+echo
+eval $CMD
+if [ $? -ne 0 ]; then REASON=1; MSG="failed up VM "$VM""; fi
+
+#			-drive vfio-pci,host=00:07.0,multifunction=on
+echo $MSG
+echo $REASON
+exit $REASON
+#END
--- gen2/vms/run_vm1.sh
+++ gen2/vms/run_vm1.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #start VM1
-RESON=0
-MSG="All done, VM "$NAME" is up..."
 
 NAME=vm1
 RAM_MB=256M
@@ -9,25 +7,29 @@ MAX_RAM_MB=512M
 CORE_COUNT=1
 VM_RAW=/VMs/HD/VM1.raw
 KERNEL=/VMs/kernel/vmlinuz-5.10.52-gentoo
-ETH0_NAME="$NAME"_eth0
-ETH1_NAME="$NAME"_eth1
+ETH0_NAME=$NAME"_eth0"
+ETH1_NAME=$NAME"_eth1"
+
+REASON=0
+MSG="All done, VM "$NAME" is up..."
 
 CMD="qemu-system-x86_64 -enable-kvm \
 			-cpu host \
 			-name $NAME \
 			-nographic \
 			-smp $CORE_COUNT \
-			-m size=$RAM_MB,maxmem=$MAX_RAM_MB \
-			-device e1000,netdev=vm1_eth0,mac=08:00:27:00:01:01
-			-netdev tap,id=net0 &
+			-m size=$RAM_MB,maxmem=$MAX_RAM_MB
+			-drive file=$VM_RAW,format=raw \
+			-kernel $KERNEL \
+			-append \"root=/dev/sda rw net.ifnames=0\" \
+			-nic tap,ifname=$ETH0_NAME,mac=08:00:27:00:01:01,script=no,downscript=no \
+			-nic tap,ifname=$ETH1_NAME,mac=08:00:27:00:01:02,script=no,downscript=no &
 "
-#			-nic tap,ifname=$ETH0_NAME,mac=08:00:27:00:01:01,script=no,downscript=no \
-#			-nic tap,ifname=$ETH2_NAME,mac=08:00:27:00:01:02,script=no,downscript=no &
-#"
+
 echo $CMD
 echo
 eval $CMD
-if [ $? -ne 0 ]; then REASON=1; MSG="failed up VM "$VM""; fi
+if [ $? -ne 0 ]; then REASON=1; MSG="failed up VM "$NAME""; fi
 
 #			-drive vfio-pci,host=00:07.0,multifunction=on
 echo $MSG
